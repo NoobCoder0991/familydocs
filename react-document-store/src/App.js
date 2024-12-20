5e2d76fb810ef02208659e0eabacd7c74286887e
shafaat Hussain <shafaath508@gmail.com>
2024-12-20 16:19:13 +0530

    Minor commit

--- react-document-store/src/App.js
+++ react-document-store/src/App.js
@@ -14,6 +14,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<Master />} />
                 <Route path="/login" element={<LoginPage />} />
+                <Route path="*" element={<Master />} />
             </Routes>
         </Router>
     )
